43267b0513d490718a6d91de8c5fe8d398ab6fd1
Anamicacodes <anamicagupta246@gmail.com>
2026-09-18 15:34:05 +0530

    Deleting elements in arrays

---
+++ DSA_practice/03_Deletion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int arr[100];
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int pos;
+    cout << "Enter the position at which you want to delete the element : ";
+    cin >> pos;
+    for(int i=pos; i<n-1;i++){
+        arr[i] = arr[i+1];
+    }
+    n--;
+    cout << "Array after deletion : " ;
+    for(int i=0;i <n ; i++){
+        cout << arr[i] << " ";
+    }
+}
