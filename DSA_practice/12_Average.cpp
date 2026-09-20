ecbbc42784a5e42c4ca8ea1ca149c4d14acaa605
Anamicacodes <anamicagupta246@gmail.com>
2026-09-20 13:49:23 +0530

    Average of the elements in the array

---
+++ DSA_practice/12_Average.cpp
@@ -0,0 +1,19 @@
+//Average of elements of the array: 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout <<"Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int sum=0 ;
+    for(int i=0; i<n;i++){
+        sum+=arr[i];
+    }
+    int average= sum/n;
+    cout <<"Average of elements of the array is: "<< average;
+}
