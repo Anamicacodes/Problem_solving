60504c2fef1ac397fc446a03ca99850f9dd0aa56
Anamicacodes <anamicagupta246@gmail.com>
2026-09-23 07:12:48 +0530

    Another approach for second largest

--- DSA_practice/18_sec_max.cpp
+++ DSA_practice/18_sec_max.cpp
@@ -31,4 +31,22 @@ int main(){
             break;
         }
     }
+
+    int max1 = arr[0];
+    int max2 = arr[1];
+    if(max2 > max1){
+        int temp = max1;
+        max1 = max2;
+        max2 = temp;
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i] > max1){
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if(arr[i]> max2 && arr[i]!=max1){
+            max2 = arr[i];
+        }
+    }
+    cout << "\nHence the second largest element is  : " << max2;
 }
