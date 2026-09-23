60ba232fc777484bb127366b4d05eccf750fa6a7
Anamicacodes <anamicagupta246@gmail.com>
2026-09-23 07:04:52 +0530

    Finding swcond maximum

---
+++ DSA_practice/18_sec_max.cpp
@@ -0,0 +1,34 @@
+//Find the second largest element of the arrays : 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int arr[100];
+    cout << "Enter n:";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    //using bubble sort: 
+    for(int i=0; i<n-1; i++){
+        for(int  j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    cout << "Sorted array : ";
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "Second largest element :" ;
+    for(int i=n-2; i>0; i--){
+        if (arr[i] < arr[n-1]){
+            cout << arr[i];
+            break;
+        }
+    }
+}
